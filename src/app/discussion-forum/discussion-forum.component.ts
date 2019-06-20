0a421ab30042ea13e5cfc3f150c9456871f58819
Dushyant Sharma <dushyant.sharma1997@gmail.com>
2019-06-20 16:33:43 +0530

    Discussion Forum changed

--- src/app/discussion-forum/discussion-forum.component.ts
+++ src/app/discussion-forum/discussion-forum.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Signup } from '../../model/signup';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-discussion-forum',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscussionForumComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  sg1=<Signup>{};
+
+  condition : boolean = true;
+  obj : Object; 
 
   ngOnInit() {
+    this.http.get('http://localhost:3000/api/DiscussionForums').subscribe((res)=>{
+      this.sg1=res as any; 
+      console.log(this.sg1);
+      this.obj = this.sg1;
+    });
   }
-
 }
